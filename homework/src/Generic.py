5cf9ffe5a63092aa3dc480812932e0b2fe01b254
ASEDOV89 <a.sedov@x5.ru>
2024-08-19 17:30:16 +0300

    TypeVar

--- homework/src/Generic.py
+++ homework/src/Generic.py
@@ -4,6 +4,9 @@ TODO:
 The function `add` accepts two arguments and returns a value, they all have the same type.
 """
 
+from typing import TypeVar
+
+T = TypeVar('T', bound=int)
 
 def add[T](a:T, b:T) -> T:
     return a
