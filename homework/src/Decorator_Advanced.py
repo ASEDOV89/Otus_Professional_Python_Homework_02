8a06e8cb69bf485d05cbd92c3d7839f5d1821a1e
ASEDOV89 <a.sedov@x5.ru>
2024-08-19 18:35:48 +0300

    Декоратор-адванс

--- homework/src/Decorator_Advanced.py
+++ homework/src/Decorator_Advanced.py
@@ -6,8 +6,17 @@ The decorator takes an argument `message` of type string
 """
 
 
-from typing import TypeVar
-from collections.abc import Callable
+from typing import TypeVar, Callable, Any
 
-def decorator[T: Callable](message:str) -> Callable[[T], T]:
-    ...
+T = TypeVar('T')
+
+def decorator(func: Callable[..., T]) -> Callable[..., T]:
+    def wrapper(*args: Any, **kwargs: Any) -> T:
+        return func(*args, **kwargs)
+    return wrapper
+
+# from typing import TypeVar
+# from collections.abc import Callable
+#
+# def decorator[T: Callable](message:str) -> Callable[[T], T]:
+#     ...
